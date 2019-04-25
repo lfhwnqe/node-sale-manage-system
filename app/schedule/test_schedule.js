a9640a8b3ee6c3edf64f9ddd28a6181312099287
lfhwnqe <464385492@qq.com>
2019-04-25 16:36:22 +0800

    定时任务：修改邮件发送的excel名称

--- app/schedule/test_schedule.js
+++ app/schedule/test_schedule.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const nodemailer = require('nodemailer');
-const auth = require('../../user_config')
+const auth = require('../../user_config');
 const xlsx = require('node-xlsx');
-const fs = require('fs')
+const fs = require('fs');
 const moment = require('moment');
-
-
-
+moment.locale('zh-cn');
 
 
 const time = 1000 * 60 * 60 * 24;
@@ -22,35 +20,38 @@ module.exports = {
     const params = {
       // startTime: moment().startOf('day').format()
       startTime: moment(new Date()).add(-1, 'days').format(),
-    }
-    const data = await ctx.service.order.totalRevenueStatics(params)
+    };
+    const data = await ctx.service.order.totalRevenueStatics(params);
     // 获取所有数据
     const {
       orderList
     } = await ctx.service.order.getOrderList({
       saleTimeStart: moment().startOf('day').format(),
       pageSize: 999
-    })
+    });
     const orderListExcelData = orderList.map((item, index) => {
-      return [index + 1, item.productName, item.amount, item.totalPrice, item.tagPrice, moment(item.saleTime).format(), item.remark]
-    })
+      return [index + 1, item.productName, item.amount, item.totalPrice, item.tagPrice, moment(item.saleTime)
+        .format(), item.remark];
+    });
     const excelData = [
       ['序号', '商品名称', '商品数量/斤', '实际出售价格/元', '吊牌价格/元', '出售时间', '备注'],
       ...orderListExcelData,
       ['', '', '', '', '', '总量/斤', '总价格/元'],
       ['', '', '', '', '', data.totalAmount, data.totalCount]
-    ]
-    console.log('excelData:', excelData)
+    ];
+    console.log('excelData:', excelData);
     const buffer = xlsx.build([{
       name: "mySheetName",
       data: excelData
-    }])
-    
+    }]);
 
-    const fileName = `./data_files/${new Date().toLocaleDateString()}销售数据统计.xlsx`
+    const fileTime = moment().format('LL');
+
+
+    const fileName = `./data_files/${fileTime}销售数据统计.xlsx`;
     fs.writeFile(fileName, buffer, (err => {
-      if (err) throw err
-    }))
+      if (err) throw err;
+    }));
 
     // // 邮件发送设置
     const transporter = nodemailer.createTransport({
@@ -67,11 +68,11 @@ module.exports = {
 
     const mailOptions = {
       from: auth.email, // sender address
-      to: auth.sendTo[0], // list of receivers
-      subject: `${new Date().toLocaleDateString()}销售数据统计`, // Subject line
+      to: auth.sendTo, // list of receivers
+      subject: `${fileTime}销售数据统计`, // Subject line
       // 发送text或者html格式
       // text: 'Hello world?', // plain text body
-      html: `${new Date().toLocaleDateString()}销售数据统计`, // html body
+      html: `${fileTime}销售数据统计`, // html body
       attachments: [{
         path: fileName
       }]
@@ -81,7 +82,7 @@ module.exports = {
       if (error) {
         return console.log(error);
       }
-      const time = new Date()
+      const time = new Date();
       console.log('Message sent: %s', info.messageId, time);
       // Message sent: <04ec7731-cc68-1ef6-303c-61b0f796b78f@qq.com>
 
