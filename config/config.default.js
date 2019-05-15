447495397941db201dea74a4c15013aee5dd36a8
lfhwnqe <464385492@qq.com>
2019-05-15 23:58:38 +0800

    日志： 修改日志文件名称

--- config/config.default.js
+++ config/config.default.js
@@ -16,6 +16,10 @@ module.exports = appInfo => {
     options: {},
   };
 
+  config.logger = {
+    appLogName:'app-nuo-web.log'
+  };
+
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1555131754056_7256';
 
